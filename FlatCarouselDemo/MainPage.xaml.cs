f2e947b95b0180c80664a50b11058d3c7ff85f5a
Marc Unterseh <marc@chloro.eu>
2014-10-02 09:09:47 +0200

    initial commit

---
+++ FlatCarouselDemo/MainPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+
+namespace FlatCarouselDemo
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        // Constructor
+        public MainPage()
+        {
+            InitializeComponent();
+            Carousel.ListImagesCarousel = new Dictionary<string, string>
+            {
+                {"/Images/1.jpg", "Image1"},
+                 {"/Images/2.jpg", "Image2"},
+                  {"/Images/3.jpg", "Image3"},
+                   {"/Images/4.jpg", "Image4"},
+                    {"/Images/5.jpg", "Image5"},
+            };
+        }
+
+
+    }
+}
---
+++ FlatCarouselWindowsPhone/UCCarouselSelection.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Navigation;
+using Microsoft.Phone.Reactive;
+using Microsoft.Phone.Shell;
+
+namespace FlatCarouselWindowsPhone
+{
+    public partial class UCCarouselSelection : UserControl
+    {
+        public UCCarouselSelection()
+        {
+            InitializeComponent();
+            Percentage = new Subject<double>();
+            Percentage.Subscribe(p =>
+            {
+                var opacity = 1 - (p/50);
+                var scale = 1 - (p/50); 
+                Ellipse.RenderTransform = new CompositeTransform{ScaleX = scale, ScaleY = scale};
+                Ellipse.Opacity = opacity; 
+            });
+        }
+
+        public static DependencyProperty ColorProperty =
+          DependencyProperty.Register("Color", typeof(Brush), typeof(UCCarouselSelection), new PropertyMetadata(new SolidColorBrush(Colors.White)));
+
+        public Brush Color
+        {
+            get
+            {
+                return (Brush)GetValue(ColorProperty);
+            }
+            set
+            {
+                SetValue(ColorProperty, value);
+            }
+        }
+
+
+        public Subject<double> Percentage { get; set; }
+
+        public void Select()
+        {
+            UnSelectAnimation.Stop();
+            SelectAnimation.Begin(); 
+        }
+
+        public void UnSelect()
+        {
+            SelectAnimation.Stop();
+            UnSelectAnimation.Begin();
+        }
+    }
+}
